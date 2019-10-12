76bf8c2b9bd218f2317bb0e861493f033fb1022b
Paul Beusterien <paulbeusterien@google.com>
2019-10-12 12:21:26 -0700

    Initial Commit

---
+++ CatalystArchiveGRPC/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CatalystArchiveGRPC
+//
+//  Created by Paul Beusterien on 10/12/19.
+//  Copyright © 2019 Paul Beusterien. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
